7e53ac2079d0097b649554899b9d2c7d61a70164
Pedro Fedricci <pedromfedricci@gmail.com>
2025-12-02 01:03:31 -0300

    nix(steam): disable open ports

--- modules/nixos/steam/default.nix
+++ modules/nixos/steam/default.nix
@@ -2,8 +2,8 @@
   programs.steam = {
     enable = true;
     protontricks.enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = false; # Open ports in the firewall for Steam Local Network Game Transfers
   };
 }
